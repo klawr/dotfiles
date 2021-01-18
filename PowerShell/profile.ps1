fca7c5ea0e4ef4753b797bcff4b3fe515971b038
Kai Lawrence <mail@kailawrence.de>
2021-01-18 14:11:13 +0100

    Replace msedge with canary

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -33,7 +33,7 @@ $paths = @(
     "$env:ProgramData\Miniconda3\Scripts\conda.exe"
     "$env:ProgramFiles\Mozilla Firefox\firefox.exe"
     "$env:OneDrive\Documents\nvim-win64\Neovim\bin\nvim.exe"
-    "$env:ProgramFiles (x86)\Microsoft\Edge\Application\msedge.exe"
+    "$env:LocalAppData\Microsoft\Edge SxS\Application\msedge.exe"
 )
 
 foreach ($path in $paths) {
