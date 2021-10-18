b4a823050fe83bcd72582de7b057341694141ea5
Kai Lawrence <mail@kailawrence.de>
2021-10-18 20:29:56 +0200

    Delete LaTeX directory

--- LaTeX/latexdocker.cmd
+++
@@ -1,4 +0,0 @@
-:; echo "Running on *nix";  docker run -i --rm -w /data -v "$(pwd):/data" tianon/latex $*; exit $?
-@ECHO OFF
-ECHO Running on Windows
-docker run -i --rm -w /data -v "%cd%:/data" tianon/latex %*
